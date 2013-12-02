5ff84bf4f1dc8e64919dac592b40180dc5c0208a
Robert Parrott <parrott@zephyr.local>
2013-12-02 12:02:58 -0500

    also include generated HASH for swift.

--- roles/packstack/files/create-packstack-passwords.sh
+++ roles/packstack/files/create-packstack-passwords.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-packstack --gen-answer-file=/tmp/answers.txt
-grep "_PW="     /tmp/answers.txt >  /tmp/packstack-passwords.txt 
-grep "_TOKEN="  /tmp/answers.txt >> /tmp/packstack-passwords.txt 
-grep "_SECRET=" /tmp/answers.txt >> /tmp/packstack-passwords.txt   
-rm /tmp/answers.txt
-        
+packstack --gen-answer-file=/tmp/answers.tmp
+grep "_PW="     /tmp/answers.tmp >  /tmp/packstack-passwords.txt
+grep "_TOKEN="  /tmp/answers.tmp >> /tmp/packstack-passwords.txt
+grep "_SECRET=" /tmp/answers.tmp >> /tmp/packstack-passwords.txt
+grep "_HASH=" /tmp/answers.tmp >> /tmp/packstack-passwords.txt
+rm /tmp/answers.tmp
