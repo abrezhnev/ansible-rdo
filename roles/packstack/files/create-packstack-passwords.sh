de377b27f4d1c0c9d2a449bb60660feac4339279
Rob Parrott <robparrott@gmail.com>
2014-05-05 11:46:39 -0400

    update packstack configs for icehouse version.

--- roles/packstack/files/create-packstack-passwords.sh
+++ roles/packstack/files/create-packstack-passwords.sh
@@ -4,5 +4,6 @@ packstack --gen-answer-file=/tmp/answers.tmp
 grep "_PW="     /tmp/answers.tmp >  /tmp/packstack-passwords.txt
 grep "_TOKEN="  /tmp/answers.tmp >> /tmp/packstack-passwords.txt
 grep "_SECRET=" /tmp/answers.tmp >> /tmp/packstack-passwords.txt
-grep "_HASH=" /tmp/answers.tmp >> /tmp/packstack-passwords.txt
+grep "_HASH="   /tmp/answers.tmp >> /tmp/packstack-passwords.txt
+grep "_KEY="    /tmp/answers.tmp >> /tmp/packstack-passwords.txt
 rm /tmp/answers.tmp
